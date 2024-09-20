6b2bf9abfbdbdc913eb34c506f0759c9dbbf5e1b
wonderguy <wonderguy88@outlook.com>
2024-09-20 18:06:11 +0400

    Add super server functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
-import { Text, Center} from '@chakra-ui/react';
+import { Text, Center } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
     <>
-      <Center borderTopEndRadius='50%' mt='8' py='20px' bg='pink.700' color='white'>
-        <Text fontSize='15px'>Copyright &copy; 2022. All rights reserved.</Text>
+      <Center
+        borderTopEndRadius="50%"
+        mt="8"
+        py="20px"
+        bg="pink.700"
+        color="white"
+      >
+        <Text fontSize="15px">Copyright &copy; 2024. All rights reserved.</Text>
       </Center>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
